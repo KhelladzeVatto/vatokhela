375bb1ca17476b6f0964ff4ecc88289e15cf3fd8
KhelladzeVatto <kheladzevato00@gmail.com>
2023-04-08 02:37:05 +0400

    first comment

---
+++ day 1/leeson1.py
@@ -0,0 +1,111 @@
+from turtle import *
+
+#we want to paint house
+
+#step 1: draw a square
+color("purple")
+width(5) #sisqe
+speed(17) #sichqare
+
+forward(200) # kubi
+left(90)
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(200)
+left(90)
+
+
+
+
+forward(70) #karebi
+color("yellow")
+begin_fill()
+left(90)
+forward(120)
+right(90)
+forward(60)
+right(90)
+forward(120)
+end_fill()
+
+penup()  #saxuravis keteba
+goto(200,200)
+pendown()
+
+color("red")
+begin_fill()
+right(150)
+forward(200)
+left(120)
+forward(200)
+end_fill()
+
+#1 window
+penup()
+goto(20,140)
+pendown()
+
+color("blue")
+left(120)
+forward(50)
+left(90)
+forward(50)
+left(90)
+forward(50)
+left(90)
+forward(50)
+left(90)
+
+forward(25)
+left(90)
+forward(50)
+
+left(90)
+forward(25)
+left(90)
+forward(25)
+left(90)
+forward(50)
+
+
+
+
+
+
+#window 2
+
+penup()
+goto(130,140)
+pendown()
+
+
+
+
+forward(50)
+left(90)
+forward(50)
+left(90)
+forward(50)
+left(90)
+forward(50)
+left(90)
+
+forward(25)
+left(90)
+forward(50)
+
+left(90)
+forward(25)
+left(90)
+forward(25)
+left(90)
+forward(50)
+
+
+
+
+
+exitonclick()
+
